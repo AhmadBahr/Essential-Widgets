1bb88544bcbe7fb62920f8e87d1166318194d5c3
Ahmad <ahmad.bahr@mubs.edu.lb>
2024-11-05 12:59:11 +0200

    start of project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Bookmark from './Components/Bookmark Extension/Bookmarks'
+import Todo from './Components/Todo Extension/Todo';
+import Crypto from './Components/Crypto Extension/Crypto';
+import Weather from './Components/Weather Extension/Weather';
+import Greeting from './Components/Greeting Extension/Greeting';
+import Time from './Components/Time Extension/Time';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <div className="App">
+        <div className="item1">
+          <Bookmark />
+        </div>
+        <div className="item2">
+          <Time />
+        </div>
+        <div className="item3">
+          <Greeting />
+        </div>
+        <div className="item4">
+          <Weather />
+        </div>
+        <div className="item5">
+          <Todo />
+        </div>
+        <div className="item6">
+          <Crypto />
+        </div>
+      </div>
     </div>
   );
 }
